735ffe6815f3f09db50ec2040e0146d0e5a6e381
arartawil <arartawil@yahoo.com>
2019-08-08 09:41:42 +0300

    Test
    Cubr_Game

---
+++ Assets/code/Camera_Move.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Move : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offset;
+
+    private void Update() {
+        transform.position= player.transform.position+offset;
+    }
+}
---
+++ Assets/code/player_move.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class player_move : MonoBehaviour
+{
+    Rigidbody rigidbody;
+
+    public int speed;
+
+    public int speed_Slide;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody=GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() 
+    {
+        rigidbody.AddForce(0,0,speed*Time.deltaTime);
+
+         if(Input.GetKey("a" ))
+         {
+            rigidbody.AddForce(-speed_Slide*Time.deltaTime,0,0,ForceMode.VelocityChange);
+         }
+         else if(Input.GetKey("d"))
+          {
+         rigidbody.AddForce(speed_Slide*Time.deltaTime,0,0,ForceMode.VelocityChange);
+          }
+
+          if(transform.position.y <=0)
+          {
+              SceneManager.LoadScene(0);
+          }
+        
+    }
+
+
+    public void Move_Rigth()
+    {
+        rigidbody.AddForce(speed_Slide*Time.deltaTime,0,0,ForceMode.VelocityChange);
+
+    }
+
+    public void Move_left()
+    {
+        rigidbody.AddForce(-speed_Slide*Time.deltaTime,0,0,ForceMode.VelocityChange);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+     {
+        if(other.tag=="C")
+        {
+            SceneManager.LoadScene(0);
+        }
+        else if(other.tag=="end")
+        {
+              SceneManager.LoadScene(1);
+        }
+    }
+       
+}
